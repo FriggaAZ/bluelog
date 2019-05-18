4dcadd8570a1edc05b2438934d54a18d65317362
FriggaAZ <FriggaAZ@outlook.com>
2019-05-19 05:36:59 +0800

    -

--- bluelog/settings.py
+++ bluelog/settings.py
@@ -63,5 +63,5 @@ class ProductionConfig(BaseConfig):
 config = {
     'development': DevelopmentConfig,
     'testing': TestingConfig,
-    # 'production': ProductionConfig
+    'production': ProductionConfig
 }
